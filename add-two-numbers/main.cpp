69e75bdf98aac6bd8e5209aeb9d463e50fd0b387
Shadman Muhtasim Ariq <shadmanmuhtasim2005@gmail.com>
2021-01-11 19:17:05 +0600

    changed to vector

--- add-two-numbers/main.cpp
+++ add-two-numbers/main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
     int a, b, t;
+    vector <int> d;
 
     cin >> t;
 
-    int z[t];
+    // int z[t];
 
-    for (int i = 0; i < t; i++){cin >> a >> b; z[i] = a + b;}
+    for (int i = 0; i < t; i++){cin >> a >> b; d.push_back(a + b);}
 
-    for (int i = 0; i < t; i++){cout << z[i] << endl;}
+    for (int i = 0; i < t; i++){cout << d[i] << endl;}
 
     return 0;
 }
