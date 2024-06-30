d2c14dd65ac1ea92b8ca867b9a9ef37ed120c4c9
Ariq100 <shadmanmuhtasim2005@gmail.com>
2024-06-30 23:40:42 +0600

    Compelted

--- Quiz Algorithm/main.cpp
+++ Quiz Algorithm/main.cpp
@@ -8,13 +8,13 @@ vector<string> answers;
 int InputQuestionsAnswers()
 {
     string input;
-    int QeustionNum;
+    int NumOfQuestions = 0;
 
     while (input != "Done")
     {
 
         cout << "Enter a question." << endl;
-        cin << input;
+        cin >> input;
 
         if(input == "Done")
         {
@@ -22,28 +22,64 @@ int InputQuestionsAnswers()
         }
 
         questions.push_back(input);
-        QeustionNum = QeustionNum + 1;
+        NumOfQuestions = NumOfQuestions + 1;
 
         cout << "Enter the answer to the question.\n" << endl;
-        cin << input;
+        cin >> input;
 
         answers.push_back(input);
+        system("cls");
     }
 
-    return QeustionNum;
+    return NumOfQuestions;
 }
 
-bool MatchAnswers(string InputAnswer)
+bool MatchAnswers(string InputAnswer, int QuestionNum)
 {
-    if(InputAnswer == )
-}
+    if(InputAnswer == answers[QuestionNum])
+    {
+        cout << "Correct Answer!!" << endl;
+        return true;
+    }
+    else 
+    {
+        Cout << "Wrong Answer!!" << endl;
+        cout << "The Correct Answer to the question is " << answers[QuestionNum] << endl;
+        return false;
+    }
 
-void()
+    system("cls");
+}
 
 int main()
 {
-    cout << "Input questions and answers and after you finish type "Done"." << endl;
+    int NumOfQuestions, marks;
+    bool decision;
+    string input;
+    cout << "Input questions and answers and after you finish type 'Done'." << endl;
     cout << "Note that Done should be typed as it is!!" << endl; 
 
-    
+    NumOfQuestions = InputQuestionsAnswers();
+
+    cout << "Answer the questions" << endl;
+
+    for(int index = 0; index < NumOfQuestions; index++)
+    {
+        cout << "Question:" << endl;
+        cout << questions[index] << endl;
+        cout << "Answer:" << endl;
+        cin >> input;
+
+        decision = MatchAnswers(input, index);
+
+        if(decision == true)
+        {
+            marks = marks + 1;
+        }
+    }
+
+    system("cls");
+    cout << "You scored a total mark of " << mark << " out of " << NumOfQuestions << endl;
+
+    return 0;
 }
